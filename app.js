f6ead3091b848e9ff0426f0dc56030816dd71f7c
pritam kundu <pritamkundu60@gmail.com>
2023-06-13 14:22:25 +0530

    collection issue may be solved

--- app.js
+++ app.js
@@ -2,20 +2,24 @@ const fastify = require('fastify')({
     logger: true
 })
 
+const fastifyMongodb = require("@fastify/mongodb");
 // const dbConnector = require('./our-db-connector')
-const firstRoute = require('./our-first-route')
-
-// fastify.get('/', (request, response) => {
-//     response.send({ hello: 'world' })
-// })
+const firstRoute = require("./our-first-route");
 
 // fastify.register(dbConnector)
 
+fastify.register(require("@fastify/mongodb"), {
+  url: "mongodb+srv://foreverabhi2002:abhi2002@cluster0.7tnrkxt.mongodb.net/?retryWrites=true&w=majority",
+});
 
+fastify.get("/test", (request, response) => {
+  const db = fastify.mongo.client.db("test");
+  const collection = db.collection("tests");
 
-fastify.register(require('@fastify/mongodb'), {
-  url: 'mongodb+srv://foreverabhi2002:abhi2002@cluster0.7tnrkxt.mongodb.net/?retryWrites=true&w=majority/test'
-})
+  const result = collection.find().toArray();
+  console.log("collection", collection);
+  response.send(result);
+});
     
     
 // dbConnector();
